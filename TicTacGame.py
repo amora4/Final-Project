7584c31fac08a973328ced0727c39de977f75236
amora4 <abhirupmora4@gmail.com>
2021-07-16 18:00:29 -0700

    Deleted TicTacGame.py

--- TicTacGame.py
+++
@@ -1,8 +0,0 @@
-from TicTacClasses import Game, Player, Board
-
-print('Welcome to Tic Tac Toe!')
-print('')
-player1 = Player('1')
-player2 = Player('2')
-game = Game(player1, player2)
-game.play()
